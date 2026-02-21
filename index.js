b73bc8efaf30a062a22bf3090f9ea5dd23a01ea5
sonam wangdus <144471374+WangdusGitHub@users.noreply.github.com>
2026-02-21 12:36:59 +0530

    connection to MongoDB, Project Setup and dotenv

--- index.js
+++ index.js
@@ -1,13 +1,20 @@
 const express = require('express');
-const app = express();
-const PORT = 8081;
-
-const {users} = require('./data/users.json');
-const {books} = require('./data/books.json');
+const dotenv = require('dotenv');
 
 // importing the routers
 const usersRouter = require("./routes/user");
 const booksRouter = require("./routes/books");
+const dbConnection = require('./dotabaseConnection');
+
+dotenv.config();
+const app = express();
+const PORT = 8081;
+dbConnection();
+
+// const {users} = require('./data/users.json');
+// const {books} = require('./data/books.json');
+
+
 
 app.use(express.json());
 
